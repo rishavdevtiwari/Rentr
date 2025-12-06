192a6cd8202b7add12d4a0cc85fe86d46aaf3e53
rishavdevtiwari <rishavtiwari.ai@gmail.com>
2025-12-06 20:15:17 +0545

    Revert "fil your profile formation"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,9 +42,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("com.airbnb.android:lottie-compose:6.0.1")
-    implementation("androidx.compose.material:material-icons-extended:1.6.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
