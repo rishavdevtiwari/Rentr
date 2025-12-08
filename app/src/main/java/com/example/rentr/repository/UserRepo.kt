903774ac67edd9239c4495139bc6041b6f1637fa
Nischal Gautam <gautamnischal2061@gmail.com>
2025-12-08 13:28:30 +0545

    set up firebase for our database system

---
+++ app/src/main/java/com/example/rentr/repository/UserRepo.kt
@@ -0,0 +1,38 @@
+package com.example.rentr.repository
+
+import com.example.rentr.model.UserModel
+import com.google.firebase.auth.FirebaseUser
+
+interface UserRepo {
+
+//   {
+//        "Success" : true
+//        "message " : "Login success"
+//    }
+    fun login(email: String, password: String,
+              callback :(Boolean, String) -> Unit)
+    fun register(email: String, password: String, callback: (Boolean, String) -> Unit)
+    fun forgetPassword(email: String,
+                       callback: (Boolean, String) -> Unit)
+    fun updateProfile(userId: String, model: UserModel,
+                      callback: (Boolean, String) -> Unit)
+    fun getCurrentUser() : FirebaseUser?
+    fun logout(callback: (Boolean, String) -> Unit)
+    fun getUserById(userId: String, callback:(Boolean,String, UserModel?) -> Unit)
+    fun getAllUsers(callback:(Boolean, String, List<UserModel>) -> Unit)
+    fun deleteAccount(userId: String, callback:(Boolean, String) -> Unit)
+
+    fun addUserToDatabase(
+        userId: String,
+        model:UserModel,
+        callback:(Boolean, String) -> Unit
+    )
+}
+
+interface ProductRepo {
+    fun addProduct()
+    fun updateProduct()
+    fun deleteProduct()
+    fun getProductById()
+    fun getAllProducts()
+}
