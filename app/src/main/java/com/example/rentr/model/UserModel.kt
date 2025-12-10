7f6bb15bd16f2c39c3e737373eba71675030b0ff
Nischal Gautam <gautamnischal2061@gmail.com>
2025-12-10 12:52:03 +0545

    added userrepoimp1 which implements all the userRepo functions

--- app/src/main/java/com/example/rentr/model/UserModel.kt
+++ app/src/main/java/com/example/rentr/model/UserModel.kt
@@ -5,6 +5,20 @@ data class UserModel(
     val firstName: String="",
     val lastName: String = "",
     val password: String = "",
-    val  gender : String = "",
-    val phoneNumber : String = ""
+    val gender : String = "",
+    val phoneNumber : String = "",
+    val uId : String = ""
 )
+{
+    fun toMap() : Map < String, Any?>{
+        return mapOf(
+            "userId" to uId,
+            "FirstName" to firstName,
+            "LastName" to lastName,
+            "Email" to email,
+            "Gender" to gender,
+            "PhoneNumber" to phoneNumber
+
+        )
+    }
+}
