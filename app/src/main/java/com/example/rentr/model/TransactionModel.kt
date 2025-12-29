2a07012e1544d9a507304c7f9a8d581882f1b3e0
Pragyan Khati <pragyankhati45@gmail.com>
2025-12-29 17:53:32 +0545

    ADD | added transaction model and initialized the transactionrepo

---
+++ app/src/main/java/com/example/rentr/model/TransactionModel.kt
@@ -0,0 +1,24 @@
+package com.example.rentr.model
+
+data class TransactionModel(
+    val productId: String = "",
+    val renterId: String = "",
+    val sellerId: String = "",
+    val basePrice: Double = 0.0,
+    val rentalPrice: Double =0.0,
+    val days:Int = 0,
+    val penaltyDays:Int = 0,
+    val paymentOption:String = "",
+    val startTime:String = "",
+    val endTime:String = "",
+    val pickupLocation:String = ""
+){
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "startTime" to startTime,
+            "endTime" to endTime,
+            "rentalPrice" to rentalPrice,
+            "penaltyDays" to penaltyDays
+        )
+    }
+}
